40c32768ae59b9794f49195309f76dc3e16a97bc
Luiz Francisco <luizfk.gmail.com>
2023-06-22 05:30:12 -0300

    Codigo modificado e funcional

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 const form = document.getElementById("form-AB");
 
-function validaForm(valores) {
-    const valorA = x;
-    const valorB = y;
-    const comparar = valores.parseInt(valorA, valorB)
-    return comparar = valorA < valorB;
-}
-
-
 form.addEventListener('submit', function(e) {
-e.preventDefault()
 
-    const ValordeA = document.getElementById('campoA');
-    const ValordeB = document.getElementById('campoB');
-    if (!validaForm (ValordeA.value) < (ValordeB.value));
-    {
-        alert('teste');
+const campoA = document.getElementById('campoA');
+const campoB = document.getElementById('campoB');
+
+    // Verifica se o valor do campo B é maior que o valor do campo A
+    if (parseInt(campoB.value) < parseInt(campoA.value)) {
+    // Impede o envio do formulário se a condição não for atendida
+    e.preventDefault();
+    alert('O valor do campo B deve ser maior que o valor do campo A.');
+    } // Caso a condição esteja certa, envia mensagem de
+    else {
+    alert ("Formulário valido com sucesso!")
     }
-    
-
-})
+}
+)
 
 
 console.log (form);
-
-
 
