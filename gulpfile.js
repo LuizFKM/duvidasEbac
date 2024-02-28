430966e5057a8a8ee0300a9be135bb9504724fd0
Luiz Francisco <luizfk.gmail.com>
2024-02-28 15:33:23 -0300

    aula 5, módulo 16, finalizada

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
 function compilaSass(){
-    return gulp.src('./source/styles/*.scss') //pega os arquivos fonte
-        .pipe(sass()) // execute as funções do sass
+    return gulp.src('./source/styles/main.scss') //pega os arquivos fonte
+        .pipe(sourcemaps.init()) // iniciando o sourcemaps, mapeia os arquivos fonte
+        .pipe(sass({
+            outputStyle: 'compressed'
+        })) // execute as funções do sass
+        .pipe(sourcemaps.write('./maps')) /*cria o arquivo de mapeamento e passamos a pasta onde os arquivos estarão disponíveis
+        ele já considera o caminho onde estão os arquivos css, não precisa especificar ./build/styles/maps */
         .pipe(gulp.dest('./build/styles')) // envia para pasta destino as compilações
 }
 
@@ -32,7 +38,9 @@ function dizTchau() { // função privada. Usamos essa sem callback e podemos us
 exports.default = gulp.parallel(funcaoPadrao, dizOi); // exportando a função por meio do método default(padrão) npm run gulp
 exports.dizOi = dizOi; // exporta a função diz oi apenas se chamar npm run gul nome_da_função(dizOi)
 exports.sass = compilaSass;
-
+exports.watch = function(){
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
+}
 
 
 
